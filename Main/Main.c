cf6e51bb16b792eacd6944a6e48c4b0d36394d21
Alejandro Ríos Jasso <33302357+AlexRiosJ@users.noreply.github.com>
2018-05-04 10:28:56 -0500

    Add files via upload

---
+++ Main/Main.c
@@ -0,0 +1,154 @@
+/*
+ * Main.c
+ *
+ *  Created on: 26/04/2017
+ *      Author: is708932
+ */
+
+#include "Main.h"
+
+#define BUFFER_LEN 100
+
+int main(){
+	Bool exitFlag = FALSE;				//Exit Flag
+	do{
+		system("cls");					//Clear screen for cmd
+		setbuf(stdout, NULL);
+		Bool wf = TRUE;					//WELL-FORMED FORMULA
+		Bool syntaxErr = FALSE;
+		printf("*** ANALIZADOR DE F\xE0RMULAS BIEN FORMADAS ***\n");
+		printf("\nIntroduce una f\xA2rmula: ");
+		char sFormula[BUFFER_LEN];
+		gets(sFormula);					//gets() fails when only enter key
+		pause();
+		Queue q1 = stringToQueue(sFormula);
+		if(q1 == NULL)
+			syntaxErr = TRUE;
+
+		//Create one queue to hold the array
+		Stack A_Stack = stack_create();
+		Stack B_Stack = stack_create();
+
+		TreeNode temp = NULL, temp2 = NULL, temp3 = NULL, temp4 = NULL, root = NULL;
+		Bool thereAreParenthesis = FALSE;
+		Bool thereIsBinary 		 = FALSE;
+
+		//HERE IT BEGINS
+		while (((!queue_isEmpty(q1) || !stack_isEmpty(A_Stack)) && wf) && !syntaxErr){
+
+			//PUSH MODE
+			temp = (struct TreeNode*) queue_poll(q1);
+			while (!queue_isEmpty(q1) && temp->type != CLOSE_PA){
+				stack_push(A_Stack, temp);
+				temp = (struct TreeNode*) queue_poll(q1);
+			}
+			if(!(temp == NULL))
+				stack_push(A_Stack,temp);
+
+			//POP MODE
+			do{
+				temp2 = (struct TreeNode*) stack_pop(A_Stack);
+				if(temp2 != NULL){
+					//All of this conditions are pushing the top data from A stack to the B stack
+					//When is an atomic proposition by itself
+					if(temp2->type == CLOSE_PA || temp2->isComplete == TRUE)
+						stack_push(B_Stack, temp2);
+
+					//When enter negation condition
+					else if(temp2->type == UNARY){
+						if(stack_isEmpty(B_Stack)) wf = FALSE;
+						else{
+							temp3 = (struct TreeNode*) stack_pop(B_Stack);
+							if(temp3->isComplete){
+								temp2->right = temp3;
+								temp2->isComplete = TRUE;
+								stack_push(B_Stack,temp2);
+							}
+							else wf = FALSE;
+						}
+					}
+
+					//When evaluating a binary expression
+					else if (temp2->type == BINARY){
+						if(stack_isEmpty(B_Stack) || stack_isEmpty(A_Stack)) wf = FALSE;
+						else{
+							temp3 = (struct TreeNode*) stack_pop(B_Stack);
+							temp4 = (struct TreeNode*) stack_pop(A_Stack);
+							if(temp3->isComplete && temp4->isComplete){
+								temp2->left  = temp4;
+								temp2->right = temp3;
+								temp2->isComplete = TRUE;
+								stack_push(B_Stack,temp2);
+								thereIsBinary = TRUE;
+							}
+							else wf = FALSE;
+						}
+					}
+
+					//When evaluating the node the open parentheses
+					else if(temp2->type == OPEN_PA){
+						if(stack_isEmpty(B_Stack))
+							wf = FALSE;
+						else{
+							temp3  = (struct TreeNode*) stack_pop(B_Stack);
+							if(temp3->isComplete){
+								if(stack_isEmpty(B_Stack))
+									wf = FALSE;
+								else{
+									temp4 = (struct TreeNode*) stack_pop(B_Stack);
+									if(temp4->type == CLOSE_PA){
+										stack_push(A_Stack, temp3);
+										thereAreParenthesis = TRUE;
+									}else
+										wf = FALSE;
+								}
+							}else
+								wf = FALSE;
+						}
+					}
+				}
+				else{
+					wf = FALSE;
+				}
+				if(stack_isEmpty(A_Stack))
+					root = (struct TreeNode*) stack_pop(B_Stack);
+			}while(!stack_isEmpty(B_Stack) && wf);
+			if(!implcation(thereIsBinary, thereAreParenthesis))
+				wf = FALSE;
+			thereAreParenthesis = FALSE;
+			thereIsBinary		= FALSE;
+		}
+
+		if(root != NULL){
+			if(root->data == ')')
+				wf = FALSE;
+		}
+
+		if(!wf){
+			printf("\nSYNTAX ERROR - F\xE0RMULA MAL FORMADA\n");
+			stack_destroy(A_Stack);
+			stack_destroy(B_Stack);
+			if(root != NULL)
+				tree_destroy(root);
+			queue_destroy(q1);
+		} else if(syntaxErr){
+			printf("\nSYNTAX ERROR - CARACTERES INV\xB5LIDOS\n");
+			stack_destroy(A_Stack);
+			stack_destroy(B_Stack);
+		} else{
+			printf("\n\n* F\xE0RMULA BIEN FORMADA *\n");
+			printf("Notaci\xA2n polaca inversa: ");
+			printPostfix(root);
+			printf("\n");
+			printf("\xB5rbol binario generado:\n");
+			printTree(root, 2);
+			stack_destroy(A_Stack);
+			stack_destroy(B_Stack);
+			tree_destroy(root);
+			queue_destroy(q1);
+		}
+		exitFlag = outMenu();
+		pause();
+	}while(!exitFlag);
+	return 0;
+}
---
+++ Main/Main.h
@@ -0,0 +1,168 @@
+/*
+ * Main.h
+ *
+ *  Created on: 27/04/2017
+ *      Author: Cursos
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <Queue.h>
+#include <Stack.h>
+
+typedef enum {
+	OPEN_PA,
+	CLOSE_PA,
+	ATOMIC,
+	UNARY,
+	BINARY
+}NodeTreeType_t;
+
+struct TreeNode{
+	char  data;
+	struct TreeNode *left, *right;
+	NodeTreeType_t type;
+	Bool isComplete;
+};
+
+//FUNC PROTOTYPES
+typedef struct TreeNode* TreeNode;
+TreeNode treeNode_create(char);
+Queue stringToQueue(char*);
+void printPostfix(TreeNode n);
+void printTree(TreeNode, int);
+void tree_destroy(TreeNode);
+void pause();
+Bool implcation(Bool, Bool);
+Bool outMenu();
+
+TreeNode treeNode_create(char data){		//To create a treeNode
+	TreeNode n = (TreeNode) malloc(sizeof(struct TreeNode));
+	n->isComplete = FALSE;
+	if(data >= 'a' && data <= 'z'){
+		n->data = data;
+		n->type = ATOMIC;
+		n->isComplete = TRUE;
+	}else{
+		switch(data){		//These are the characters that the node can receive
+			case '\xAA':	n->data = data;	n->type = UNARY;	break;		//Hexadecimal for printing and scanning in cmd
+			case '(':		n->data = data;	n->type = OPEN_PA;	break;
+			case ')':		n->data = data;	n->type = CLOSE_PA;	break;
+			case '\x26':	n->data = data;	n->type = BINARY;	break;		//Hexadecimal for printing and scanning in cmd
+			case '\x7C':	n->data = data;	n->type = BINARY;	break;		//Hexadecimal for printing and scanning in cmd
+			case '#':		n->data = data;	n->type = BINARY;	break;
+			case '$':		n->data = data;	n->type = BINARY;	break;
+			default :	return NULL;
+		}
+	}
+	n->left  = NULL;
+	n->right = NULL;
+	return n;
+}
+
+Queue stringToQueue(char* string){			//To introduce the string into a Queue
+	Queue q1 = queue_create();
+	int i;
+	for(i = 0; i < strlen(string); i++){
+		if(string[i] == '-' && string[i+1] == '>'){ 	//To convert the expression '->' into a single char to facilitate its use
+			i++;
+			TreeNode n = treeNode_create('$');
+				if(n != NULL){
+					queue_offer(q1, n);
+				} else{
+					queue_destroy(q1);
+					return NULL;
+				}
+		}
+		else if(string[i] == '<' && string[i+1] == '-' && string[i+2] == '>'){	//To convert the expression '<->' into a single char
+			i += 2;																//to facilitate its use
+			TreeNode n = treeNode_create('#');
+			if(n != NULL){
+				queue_offer(q1, n);
+			} else{
+				queue_destroy(q1);
+				return NULL;
+			}
+		}
+		else if(string[i] != ' '){						//To ignore spaces of the string
+			TreeNode n = treeNode_create(string[i]);
+			if(n != NULL){
+				queue_offer(q1, n);
+			} else{
+				queue_destroy(q1);
+				return NULL;
+			}
+		}
+	}
+	return q1;
+}
+
+void printPostfix(TreeNode n) {							//To print the postfix notation
+	if(n->left  != NULL) printPostfix(n->left);
+	if(n->right != NULL) printPostfix(n->right);
+	if(n->data == '#')
+		printf("<->");
+	else if(n->data == '$')
+		printf("->");
+	else
+		printf("%c", n->data);
+}
+
+void printTree(TreeNode n, int spaces){					//To print the tree
+	int i;
+	for(i = 0; i < spaces; i++)
+		printf(" ");
+	if(n->data == '#'){
+		puts("<->");
+		spaces += 2;
+	} else if(n->data == '$'){
+		puts("->");
+		spaces += 2;
+	}else
+		printf("%c\n", n->data);
+	if(n->left  != NULL) printTree(n->left,  spaces + 2);
+	if(n->right != NULL) printTree(n->right, spaces + 2);
+}
+
+void tree_destroy(TreeNode n){							//To destroy the tree
+	if(n->left  != NULL) tree_destroy(n->left);
+	if(n->right != NULL) tree_destroy(n->right);
+	free(n);
+}
+
+void pause(){											//To do pauses in the .exe running
+	printf("Presiona enter para continuar . . .");
+	getchar();
+}
+
+Bool implcation(Bool a, Bool b){						//Logical implication function
+	Bool implication;
+	if(a == b)
+		implication = TRUE;
+	if(a == FALSE && b == TRUE)
+		implication = b;
+	else if(a == TRUE && b == FALSE)
+		implication = b;
+	return implication;
+}
+
+Bool outMenu(){											//Menu for out or introduce another formula
+	int opc = 0;
+	printf("\nIngresar otra f\xA2rmula\t(0)\n");
+	printf("Salir\t\t\t(1)\n");
+	printf("Opci\xA2n: ");
+	scanf("%d", &opc);
+	getchar();
+	if(opc == FALSE || opc == TRUE)
+		return opc;
+	else{
+		printf("\xADOpci\xA2n inv\xA0lida! Intenta de nuevo . . .\n");
+		return outMenu();
+	}
+}
+
+#endif /* MAIN_H_ */
